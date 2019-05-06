4bf5cc24252e10accd8a48c7fd72aeb2b527e3c4
Eika4 <38053046+Eika4@users.noreply.github.com>
2019-05-06 17:44:19 +0000

    update for petrachor repository

--- cmake/ProjectPairing.cmake
+++ cmake/ProjectPairing.cmake
@@ -5,7 +5,7 @@ set(LIBRARY_DIR "${prefix}/${CMAKE_INSTALL_LIBDIR}/")
 
 ExternalProject_Add(pairing
     PREFIX "${prefix}"
-    URL https://github.com/petrichor-dev/pairing-ariel/archive/ef052cf4fde9f6bb3a53a2114f7f8f76c13ba218.tar.gz
+    URL https://github.com/petrachor/pairing-ariel/archive/ef052cf4fde9f6bb3a53a2114f7f8f76c13ba218.tar.gz
 		URL_HASH SHA256=18c02869af73a3269c71b4bd96dc13391c5ecd1a218e08f17bd6845d926aea86
     BUILD_BYPRODUCTS "${PAIRING_LIBRARY}"
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_INSTALL_DIR=<INSTALL_DIR>
