9cc999c4bb7956d55d16ecb12a94c572c604227a
Ruell <ruellm@yahoo.com>
2021-07-20 12:41:14 +0800

    added synch subscription

--- websocket-api/subscription/SyncChangeSubscription.cpp
+++ websocket-api/subscription/SyncChangeSubscription.cpp
@@ -1,5 +1,37 @@
-//
-// Created by ruell on 20/07/2021.
-//
-
 #include "SyncChangeSubscription.h"
+#include "../WebsocketEvents.h"
+#include "../WebsocketServer.h"
+
+namespace WebsocketAPI
+{
+	SyncChangeSubscription::SyncChangeSubscription(IWebsocketClient* client)
+			: Subscription(client, Subscription::SyncChanged)
+	{
+		m_connection = WebSocketEvents::getInstance()->subscribeSyncChangeEvent(
+				std::bind(&SyncChangeSubscription::onSyncChanged, this));
+	}
+
+	void SyncChangeSubscription::onSyncChanged()
+	{
+		auto interface = WebsocketAPI::getEthInterface();
+		if(interface == nullptr)
+			return;
+
+		auto result = interface->eth_syncing();
+		Json::Value root;
+		Json::Value params;
+
+		params["subscription"] = getId();
+
+		root["jsonrpc"] = "2.0";
+		root["method"] = "eth_subscription";
+		params["subscription"] = getId();
+		params["result"] = result;
+
+		root["params"] = params;
+
+		Json::FastWriter fastWriter;
+		std::string out = fastWriter.write(root);
+		getClient()->sendSync(out);
+	}
+}
--- websocket-api/subscription/SyncChangeSubscription.h
+++ websocket-api/subscription/SyncChangeSubscription.h
@@ -1,8 +1,17 @@
-//
-// Created by ruell on 20/07/2021.
-//
+#pragma once
 
-#ifndef PETRACHOR_SYNCCHANGESUBSCRIPTION_H
-#define PETRACHOR_SYNCCHANGESUBSCRIPTION_H
+#include <string>
+#include <json/json.h>
+#include "libethcore/BlockHeader.h"
+#include "websocket-api/IWebSocketClient.h"
+#include "Subscription.h"
 
-#endif //PETRACHOR_SYNCCHANGESUBSCRIPTION_H
+namespace WebsocketAPI
+{
+	class SyncChangeSubscription : public Subscription{
+	public:
+		SyncChangeSubscription(IWebsocketClient* client);
+		void onSyncChanged();
+
+	};
+}
